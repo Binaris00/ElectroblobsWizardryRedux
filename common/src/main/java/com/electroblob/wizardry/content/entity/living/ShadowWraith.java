afd0aea7ddcb96d6e01e8849d1b87af40f91b763
Binary <binaris000@gmail.com>
2025-12-27 17:39:13 -0400

    :art: feat -> implement BlazeLikeSpellAttackGoal for various wraiths and enhance spell casting behavior for all the "blaze casting spell entities"

--- common/src/main/java/com/electroblob/wizardry/content/entity/living/ShadowWraith.java
+++ common/src/main/java/com/electroblob/wizardry/content/entity/living/ShadowWraith.java
@@ -1,22 +1,17 @@
 package com.electroblob.wizardry.content.entity.living;
 
 import com.electroblob.wizardry.api.client.ParticleBuilder;
-import com.electroblob.wizardry.api.content.spell.internal.EntityCastContext;
-import com.electroblob.wizardry.api.content.spell.internal.SpellModifiers;
-import com.electroblob.wizardry.core.mixin.accessor.BlazeAccessor;
+import com.electroblob.wizardry.content.entity.goal.BlazeLikeSpellAttackGoal;
 import com.electroblob.wizardry.core.mixin.accessor.MobGoalsAccessor;
 import com.electroblob.wizardry.setup.registries.EBEntities;
 import com.electroblob.wizardry.setup.registries.EBSounds;
 import com.electroblob.wizardry.setup.registries.Spells;
 import com.electroblob.wizardry.setup.registries.client.EBParticles;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
@@ -25,8 +20,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.EnumSet;
 
 public class ShadowWraith extends Blaze {
 
@@ -130,16 +123,6 @@ public class ShadowWraith extends Blaze {
         return super.canBeAffected(effect);
     }
 
-//    @Override
-//    public int getSkyLightLevel() {
-//        return 15; // Maximum brightness
-//    }
-//
-//    @Override
-//    public float getBrightness() {
-//        return 1.0F;
-//    }
-
     @Override
     public boolean causeFallDamage(float fallDistance, float multiplier, @NotNull DamageSource source) {
         return false;
@@ -151,97 +134,12 @@ public class ShadowWraith extends Blaze {
         this.goalSelector.removeAllGoals((goal) -> true);
         this.targetSelector.removeAllGoals((goal) -> true);
 
-        this.goalSelector.addGoal(4, new ShadowWraithAttackGoal(this));
+        this.goalSelector.addGoal(4, new BlazeLikeSpellAttackGoal(this, Spells.DARKNESS_ORB));
         this.goalSelector.addGoal(5, new MoveTowardsRestrictionGoal(this, 1.0F));
         this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0F, 0.0F));
         this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
         this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers());
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
-    }
-
-    static class ShadowWraithAttackGoal extends Goal {
-        private final ShadowWraith wraith;
-        private int attackStep;
-        private int attackTime;
-        private int lastSeen;
-
-        public ShadowWraithAttackGoal(ShadowWraith wraith) {
-            this.wraith = wraith;
-            this.setFlags(EnumSet.of(Flag.MOVE, Flag.LOOK));
-        }
-
-        public boolean canUse() {
-            LivingEntity livingentity = this.wraith.getTarget();
-            return livingentity != null && livingentity.isAlive() && this.wraith.canAttack(livingentity);
-        }
-
-        public void start() {
-            this.attackStep = 0;
-        }
-
-        public void stop() {
-            ((BlazeAccessor) wraith).callSetCharged(false);
-            this.lastSeen = 0;
-        }
-
-        public boolean requiresUpdateEveryTick() {
-            return true;
-        }
-
-        public void tick() {
-            --this.attackTime;
-            LivingEntity livingentity = this.wraith.getTarget();
-            if (livingentity != null) {
-                boolean flag = this.wraith.getSensing().hasLineOfSight(livingentity);
-                if (flag) {
-                    this.lastSeen = 0;
-                } else {
-                    ++this.lastSeen;
-                }
-
-                double d0 = this.wraith.distanceToSqr(livingentity);
-                if (d0 < (double) 4.0F) {
-                    if (!flag) {
-                        return;
-                    }
-
-                    if (this.attackTime <= 0) {
-                        this.attackTime = 20;
-                        this.wraith.doHurtTarget(livingentity);
-                    }
-
-                    this.wraith.getMoveControl().setWantedPosition(livingentity.getX(), livingentity.getY(), livingentity.getZ(), (double) 1.0F);
-                } else if (d0 < this.getFollowDistance() * this.getFollowDistance() && flag) {
-                    if (this.attackTime <= 0) {
-                        ++this.attackStep;
-                        if (this.attackStep == 1) {
-                            this.attackTime = 60;
-                            ((BlazeAccessor) wraith).callSetCharged(true);
-                        } else if (this.attackStep <= 4) {
-                            this.attackTime = 6;
-                        } else {
-                            this.attackTime = 100;
-                            this.attackStep = 0;
-                            ((BlazeAccessor) wraith).callSetCharged(false);
-                        }
-
-                        if (this.attackStep > 1) {
-                            Spells.DARKNESS_ORB.cast(new EntityCastContext(wraith.level(), wraith, InteractionHand.MAIN_HAND, 0, livingentity, new SpellModifiers()));
-                        }
-                    }
-
-                    this.wraith.getLookControl().setLookAt(livingentity, 10.0F, 10.0F);
-                } else if (this.lastSeen < 5) {
-                    this.wraith.getMoveControl().setWantedPosition(livingentity.getX(), livingentity.getY(), livingentity.getZ(), (double) 1.0F);
-                }
-
-                super.tick();
-            }
-        }
-
-        private double getFollowDistance() {
-            return this.wraith.getAttributeValue(Attributes.FOLLOW_RANGE);
-        }
     }
 }
--- common/src/main/java/com/electroblob/wizardry/content/entity/living/StormElemental.java
+++ common/src/main/java/com/electroblob/wizardry/content/entity/living/StormElemental.java
@@ -1,17 +1,17 @@
 package com.electroblob.wizardry.content.entity.living;
 
 import com.electroblob.wizardry.api.client.ParticleBuilder;
-import com.electroblob.wizardry.core.mixin.accessor.BlazeAccessor;
+import com.electroblob.wizardry.content.entity.goal.BlazeLikeSpellAttackGoal;
 import com.electroblob.wizardry.core.mixin.accessor.MobGoalsAccessor;
 import com.electroblob.wizardry.setup.registries.EBEntities;
 import com.electroblob.wizardry.setup.registries.EBSounds;
+import com.electroblob.wizardry.setup.registries.Spells;
 import com.electroblob.wizardry.setup.registries.client.EBParticles;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LightningBolt;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
@@ -19,8 +19,8 @@ import net.minecraft.world.entity.monster.Blaze;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.EnumSet;
 
 public class StormElemental extends Blaze {
 
@@ -115,23 +115,13 @@ public class StormElemental extends Blaze {
         return false;
     }
 
-//    @Override
-//    public int getSkyLightLevel() {
-//        return 15; // Maximum brightness
-//    }
-//
-//    @Override
-//    public float getBrightness() {
-//        return 1.0F;
-//    }
-
     @Override
-    public boolean causeFallDamage(float fallDistance, float multiplier, DamageSource source) {
+    public boolean causeFallDamage(float fallDistance, float multiplier, @NotNull DamageSource source) {
         return false;
     }
 
     @Override
-    public void thunderHit(net.minecraft.server.level.ServerLevel level, LightningBolt lightning) {
+    public void thunderHit(@NotNull ServerLevel level, @NotNull LightningBolt lightning) {
         // Immune to lightning strikes - do nothing
     }
 
@@ -141,98 +131,13 @@ public class StormElemental extends Blaze {
         this.goalSelector.removeAllGoals((goal) -> true);
         this.targetSelector.removeAllGoals((goal) -> true);
 
-        this.goalSelector.addGoal(4, new StormElementalAttackGoal(this));
+        // TODO LIGHTNING DISC
+        this.goalSelector.addGoal(4, new BlazeLikeSpellAttackGoal(this, Spells.LIGHTNING_ARROW));
         this.goalSelector.addGoal(5, new MoveTowardsRestrictionGoal(this, 1.0F));
         this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0F, 0.0F));
         this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
         this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
         this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers());
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
-    }
-
-    static class StormElementalAttackGoal extends Goal {
-        private final StormElemental elemental;
-        private int attackStep;
-        private int attackTime;
-        private int lastSeen;
-
-        public StormElementalAttackGoal(StormElemental elemental) {
-            this.elemental = elemental;
-            this.setFlags(EnumSet.of(Flag.MOVE, Flag.LOOK));
-        }
-
-        public boolean canUse() {
-            LivingEntity livingentity = this.elemental.getTarget();
-            return livingentity != null && livingentity.isAlive() && this.elemental.canAttack(livingentity);
-        }
-
-        public void start() {
-            this.attackStep = 0;
-        }
-
-        public void stop() {
-            ((BlazeAccessor) elemental).callSetCharged(false);
-            this.lastSeen = 0;
-        }
-
-        public boolean requiresUpdateEveryTick() {
-            return true;
-        }
-
-        public void tick() {
-            --this.attackTime;
-            LivingEntity livingentity = this.elemental.getTarget();
-            if (livingentity != null) {
-                boolean flag = this.elemental.getSensing().hasLineOfSight(livingentity);
-                if (flag) {
-                    this.lastSeen = 0;
-                } else {
-                    ++this.lastSeen;
-                }
-
-                double d0 = this.elemental.distanceToSqr(livingentity);
-                if (d0 < (double) 4.0F) {
-                    if (!flag) {
-                        return;
-                    }
-
-                    if (this.attackTime <= 0) {
-                        this.attackTime = 20;
-                        this.elemental.doHurtTarget(livingentity);
-                    }
-
-                    this.elemental.getMoveControl().setWantedPosition(livingentity.getX(), livingentity.getY(), livingentity.getZ(), (double) 1.0F);
-                } else if (d0 < this.getFollowDistance() * this.getFollowDistance() && flag) {
-                    if (this.attackTime <= 0) {
-                        ++this.attackStep;
-                        if (this.attackStep == 1) {
-                            this.attackTime = 60;
-                            ((BlazeAccessor) elemental).callSetCharged(true);
-                        } else if (this.attackStep <= 4) {
-                            this.attackTime = 6;
-                        } else {
-                            this.attackTime = 100;
-                            this.attackStep = 0;
-                            ((BlazeAccessor) elemental).callSetCharged(false);
-                        }
-
-                        if (this.attackStep > 1) {
-                            // TODO Lightning Disc Spell Casting
-                            //Spells.LIGHTNING_DISC.cast(new EntityCastContext(elemental.level(), elemental, InteractionHand.MAIN_HAND, 0, livingentity, new SpellModifiers()));
-                        }
-                    }
-
-                    this.elemental.getLookControl().setLookAt(livingentity, 10.0F, 10.0F);
-                } else if (this.lastSeen < 5) {
-                    this.elemental.getMoveControl().setWantedPosition(livingentity.getX(), livingentity.getY(), livingentity.getZ(), (double) 1.0F);
-                }
-
-                super.tick();
-            }
-        }
-
-        private double getFollowDistance() {
-            return this.elemental.getAttributeValue(Attributes.FOLLOW_RANGE);
-        }
     }
 }
