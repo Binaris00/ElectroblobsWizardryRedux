f359f897c3e1f84d74b0151baab1ec5486f0af1a
Crystal <crystal@qq.com>
2026-08-18 08:28:53 +0800

    ✅ Dependency -> Add dependency for JEI mod, Arcane Workbench, and Imbuement Altar

--- common/src/main/java/com/binaris/wizardry/client/compat/WizardryJeiPlugin.java
+++ common/src/main/java/com/binaris/wizardry/client/compat/WizardryJeiPlugin.java
@@ -10,17 +10,18 @@ import com.binaris.wizardry.setup.registries.EBItems;
 import com.binaris.wizardry.setup.registries.EBRecipeTypes;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
-import mezz.jei.api.registration.IRecipeCatalystRegistration;
-import mezz.jei.api.registration.IRecipeCategoryRegistration;
-import mezz.jei.api.registration.IRecipeRegistration;
-import mezz.jei.api.registration.ISubtypeRegistration;
+import mezz.jei.api.registration.*;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.item.enchantment.Enchantment;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Locale;
 
 @JeiPlugin
 public class WizardryJeiPlugin implements IModPlugin {
@@ -46,6 +47,40 @@ public class WizardryJeiPlugin implements IModPlugin {
 
         register.addRecipes(ImbuementAltarCategory.IMBUE_TYPE, imbuementAltarRecipes);
         register.addRecipes(ArcaneWorkbenchCategory.ARCANE_WORKBENCH, ArcaneWorkbenchCategory.generateRecipes());
+
+        // Item Info
+        register.addIngredientInfo(EBItems.MAGIC_CRYSTAL.get(), Component.translatable("item.ebwizardry.magic_crystal.desc"));
+        register.addIngredientInfo(EBItems.WIZARD_HANDBOOK.get(), Component.translatable("item.ebwizardry.wizard_handbook.desc"));
+        register.addIngredientInfo(EBItems.MAGIC_CRYSTAL_SHARD.get(), Component.translatable("item.ebwizardry.magic_crystal_shard.desc"));
+        register.addIngredientInfo(EBItems.MAGIC_CRYSTAL_GRAND.get(), Component.translatable("item.ebwizardry.magic_crystal_grand.desc"));
+        register.addIngredientInfo(EBItems.IDENTIFICATION_SCROLL.get(), Component.translatable("item.ebwizardry.identification_scroll.desc_extended"));
+        register.addIngredientInfo(EBItems.SPELL_BOOK.get(), Component.translatable("item.ebwizardry.spell_book.desc"));
+        register.addIngredientInfo(EBItems.SCROLL.get(), Component.translatable("item.ebwizardry.scroll.desc"));
+        register.addIngredientInfo(EBItems.RESPLENDENT_THREAD.get(), Component.translatable("item.ebwizardry.resplendent_thread.desc_extended"));
+        register.addIngredientInfo(EBItems.CRYSTAL_SILVER_PLATING.get(), Component.translatable("item.ebwizardry.crystal_silver_plating.desc_extended"));
+        register.addIngredientInfo(EBItems.ETHEREAL_CRYSTAL_WEAVE.get(), Component.translatable("item.ebwizardry.ethereal_crystal_weave.desc_extended"));
+        register.addIngredientInfo(EBItems.PURIFYING_ELIXIR.get(), Component.translatable("item.ebwizardry.purifying_elixir.desc_extended"));
+        register.addIngredientInfo(EBItems.ASTRAL_DIAMOND.get(), Component.translatable("item.ebwizardry.astral_diamond.desc"));
+        register.addIngredientInfo(EBItems.RUINED_SPELL_BOOK.get(), Component.translatable("item.ebwizardry.ruined_spell_book.desc"));
+
+        EBItems.getArtifacts().forEach((item, type) ->
+                register.addIngredientInfo(item.get(), Component.translatable("item.ebwizardry." + type.toString().toLowerCase(Locale.ROOT) + ".generic.desc"))
+        );
+
+        addWandUpgradeInfo(register, EBItems.BLAST_UPGRADE.get());
+        addWandUpgradeInfo(register, EBItems.ATTUNEMENT_UPGRADE.get());
+        addWandUpgradeInfo(register, EBItems.BLAST_UPGRADE.get());
+        addWandUpgradeInfo(register, EBItems.CONDENSER_UPGRADE.get());
+        addWandUpgradeInfo(register, EBItems.COOLDOWN_UPGRADE.get());
+        addWandUpgradeInfo(register, EBItems.DURATION_UPGRADE.get());
+        addWandUpgradeInfo(register, EBItems.MELEE_UPGRADE.get());
+        addWandUpgradeInfo(register, EBItems.RANGE_UPGRADE.get());
+        addWandUpgradeInfo(register, EBItems.SIPHON_UPGRADE.get());
+        addWandUpgradeInfo(register, EBItems.STORAGE_UPGRADE.get());
+        addWandUpgradeInfo(register, EBItems.ARCANE_TOME.get());
+        addWandUpgradeInfo(register, EBItems.APPRENTICE_ARCANE_TOME.get());
+        addWandUpgradeInfo(register, EBItems.ADVANCED_ARCANE_TOME.get());
+        addWandUpgradeInfo(register, EBItems.MASTER_ARCANE_TOME.get());
     }
 
     @Override
@@ -60,5 +95,13 @@ public class WizardryJeiPlugin implements IModPlugin {
         IModPlugin.super.registerItemSubtypes(register);
         register.registerSubtypeInterpreter(EBItems.SCROLL.get(), SpellSubtypeInterpreter.INSTANCE);
         register.registerSubtypeInterpreter(EBItems.SPELL_BOOK.get(), SpellSubtypeInterpreter.INSTANCE);
+    }
+
+    private void addWandUpgradeInfo(IRecipeRegistration register, Item item) {
+        register.addIngredientInfo(item, Component.translatable("item.ebwizardry.wand_upgrade.generic.desc"));
+    }
+
+    private void addEnchantmentInfo(IRecipeRegistration register, Enchantment enchantment, Component description) {
+        register.addIngredientInfo(enchantment, () -> Enchantment.class, description);
     }
 }
